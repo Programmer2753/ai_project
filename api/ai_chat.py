fd261825eddfcafd209a9de444e82321b21e3519
Programmer2753 <143874664+Programmer2753@users.noreply.github.com>
2026-01-29 19:23:47 +0200

    Add files via upload

--- api/ai_chat.py
+++ api/ai_chat.py
@@ -8,7 +8,7 @@ api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=api_key)
 
 # Выбираем стабильную и умную модель из твоего списка
-MODEL_NAME = 'models/gemini-flash-latest'
+MODEL_NAME = 'models/gemma-3-27b-it'
 
 app = FastAPI()
 
